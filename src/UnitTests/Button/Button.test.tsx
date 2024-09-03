1489fb55a83a7d3fa59404ee3644d16af823064a
Pelayo Suárez <pesuafer@gmail.com>
2024-09-03 17:13:58 +0200

    feat: :sparkles: created a basic component and its unit test

--- src/UnitTests/Button/Button.test.tsx
+++ src/UnitTests/Button/Button.test.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Button from './Button';
 
-test("Button renders successfully", () => {
-    render(<Button onClick={()=>{
-        alert('alert')
-    }}>Test Button</Button>);
+describe('Button component', () => {
+    test("should renders successfully", () => {
+        render(<Button onClick={() => { }}>Test Button</Button>);
 
-    const element = screen.getByText(/Test Button/i);
+        const element = screen.getByRole('button', {
+            name: /Test Button/i
+        });
 
-    expect(element).toBeInTheDocument();
+        expect(element).toBeInTheDocument();
+    })
+
+    test('should be disabled', () => {
+        const handleClick = jest.fn();
+        render(<Button onClick={handleClick} disabled>Test Button</Button>);
+
+        const element = screen.getByRole('button');
+        fireEvent.click(element);
+
+        expect(element).toBeDisabled();
+        
+        expect(handleClick).toHaveBeenCalledTimes(0);
+    })
+
+    test('should be loading', () => {
+        render(<Button onClick={() => { }} loading>Test Button</Button>);
+        const element = screen.getByRole('button');
+
+        expect(element).toBeDisabled();
+        expect(element).toHaveTextContent('Loading ...')
+    })
+
+    test('should activate onClick event', () => {
+        const handleClick = jest.fn();
+        render(<Button onClick={handleClick}>Test Button</Button>);
+
+        const element = screen.getByRole('button');
+        fireEvent.click(element);
+
+        expect(handleClick).toHaveBeenCalledTimes(1);
+    })
+
+    test('should have a specific type: submit', () => {
+        render(<Button type="submit">Submit</Button>);
+        const element = screen.getByRole('button', { name: /Submit/i });
+    
+        expect(element).toHaveAttribute('type', 'submit');
+    });
 })
--- src/UnitTests/Button/Button.tsx
+++ src/UnitTests/Button/Button.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
-interface props {
-    onClick: () => void,
-    children: React.ReactNode
+interface props  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode,
+    loading?: boolean,
 }
-
-function Button({onClick, children}: props) {
+function Button({ onClick, children, loading = false, disabled, type }: props) {
     return (
-        <button onClick={onClick}>{children}</button>
+        <button onClick={onClick} disabled={disabled || loading} type={type}>{loading ? 'Loading ...' : children}</button>
     )
 }
 
