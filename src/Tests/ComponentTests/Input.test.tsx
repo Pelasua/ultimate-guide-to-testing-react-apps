fdfc06bb0e3f37b4ceca80b95b0f11e6f977c2e4
Pelayo Suárez <pesuafer@gmail.com>
2024-09-12 12:07:33 +0200

    chore: :wrench: applied new eslint configurations

--- src/Tests/ComponentTests/Input.test.tsx
+++ src/Tests/ComponentTests/Input.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { prettyDOM, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Input from '../../Components/Input';
 
@@ -14,7 +14,7 @@ describe('Input component', () => {
 
             const labelElement = screen.getByText("label");
 
-            expect(labelElement).toBeInTheDocument();
+             expect(labelElement).toBeInTheDocument();
         });
     });
 
