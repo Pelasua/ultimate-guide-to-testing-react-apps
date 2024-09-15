b807becfd147bb6749f2a1237c62dc14141a546b
Pelayo Suárez <pesuafer@gmail.com>
2024-09-15 11:18:32 +0200

    feat: :white_check_mark: added a new test to digimon detail component

--- src/Tests/ComponentTests/DigimonDetail.test.tsx
+++ src/Tests/ComponentTests/DigimonDetail.test.tsx
@@ -10,26 +10,43 @@ describe('DigimonDetail component', () => {
             src: "src test",
             name: "Digimon"
         };
-    
+
         beforeEach(() => {
             render(<DigimonDetail {...props} />);
         });
-    
-        describe('Renderization and Props', () => {
-            test('should render an image with the correct attributes', () => {
-                const imgElement = screen.getByRole('img');
-                
-                expect(imgElement).toBeInTheDocument();
-                expect(imgElement).toHaveAttribute('src', props.src);
-                expect(imgElement).toHaveAttribute('alt', props.name);
-            });
-    
-            test('should display the digimon name in a span', () => {
-                const nameElement = screen.getByText(props.name, { selector: 'span' });
-                
-                expect(nameElement).toBeInTheDocument();
-                expect(nameElement).toHaveTextContent(props.name);
-            });
+
+
+        test('should render an image with the correct attributes', () => {
+            const imgElement = screen.getByRole('img');
+
+            expect(imgElement).toBeInTheDocument();
+            expect(imgElement).toHaveAttribute('src', props.src);
+            expect(imgElement).toHaveAttribute('alt', props.name);
+        });
+
+        test('should display the digimon name in a span', () => {
+            const nameElement = screen.getByText(props.name, { selector: 'span' });
+
+            expect(nameElement).toBeInTheDocument();
+            expect(nameElement).toHaveTextContent(props.name);
+        });
+
+        test('should show a spinner when loading property is true', () => {
+            render(<DigimonDetail loading />);
+            const spinnerElement = screen.getByRole('status');
+
+            expect(spinnerElement).toBeInTheDocument();
+        });
+
+    });
+
+    describe('Styles', () => { 
+        test('should have an image with a height of 13rem', () => {
+            render(<DigimonDetail src='src' name='name' />);
+            const imgElement = screen.getByRole('img');
+
+            expect(imgElement).toHaveClass('h-52');
+            
         });
     });
 });
