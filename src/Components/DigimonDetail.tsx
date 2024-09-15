d4deeb18927d67b7c8cfdc6a60254c1e173d7da2
Pelayo Suárez <pesuafer@gmail.com>
2024-09-15 11:17:49 +0200

    feat: :sparkles: added a spinner and some styles to digimon detail component

--- src/Components/DigimonDetail.tsx
+++ src/Components/DigimonDetail.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import Spinner from './Spinner';
 
 interface Props {
   src?: string
-  name?: string
+  name?: string,
+  loading?: boolean
 }
 
-function DigimonDetail({ src, name }: Props) {
+function DigimonDetail({ src, name, loading = false }: Props) {
   return (
-    <div>
-      {src && name &&
+    <div className='flex justify-center items-center gap-10'>
+
+      {loading ? <Spinner /> : src && name &&
         <>
-          <img src={src} alt={`${name}`} />
-          <span>{name}</span>
-        </>
-      }
+          <img src={src} alt={`${name}`} className='h-52' />
+          <span className='text-xl underline font-semibold'>{name}</span>
+        </>}
+
+
     </div>
   );
 }
